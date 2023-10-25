34a26e8fd50e49d7aaf4616a792a647fcbd436d8
Mario Hosnjak <mario.hosnjak009@gmail.com>
2023-10-25 22:40:22 +0200

    Configured Auth0 login

---
+++ backend/src/main/java/com/WEB2/backend/Controller/HomePageController.java
@@ -0,0 +1,36 @@
+package com.WEB2.backend.Controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
+
+   /* @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }*/
+
+    /*@GetMapping("/profile")
+    public ModelAndView profile() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("profile.html");
+        return modelAndView;
+    }*/
+
+    @GetMapping("/profile")
+    public String profile(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "profile";
+    }
+}
---
+++ backend/src/main/java/com/WEB2/backend/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.WEB2.backend;
+
+import  com.WEB2.backend.LogoutHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final LogoutHandler logoutHandler;
+
+    public SecurityConfig(LogoutHandler logoutHandler) {
+        this.logoutHandler = logoutHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*return http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/").permitAll();
+            auth.requestMatchers("/home").permitAll();
+            auth.requestMatchers("/profile").authenticated();
+        }).build();*/
+        return http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/").permitAll();
+            try {
+                auth.anyRequest().authenticated().and().oauth2Login()
+                        .and().logout()
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .addLogoutHandler(logoutHandler);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).build();
+
+    }
+}
